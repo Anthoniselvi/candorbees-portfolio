f4a981d79e7658767aed721da2dc36aae264b774
Anthoniselvi <anthoniselvi33@gmail.com>
2023-08-07 16:22:32 +0530

    logo images added

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,20 @@ import Projects from "@/components/Home/Projects";
 import WorkingWayAndStock from "@/components/Home/WorkingWayAndStock";
 import Teams from "@/components/Home/Teams";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Candorbees</title>
+        <Image
+          src="/image/logo-small.png"
+          width="50"
+          height="50"
+          alt=""
+          className={styles.image}
+        />
       </Head>
       <Header />
       <Projects />
