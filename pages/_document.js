e39c2708c49b1845a7518980b908e9d6304c85f1
Anthoniselvi <anthoniselvi33@gmail.com>
2023-08-08 18:25:10 +0530

    nav bar updated

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/image/logo-big.png" />
+        <title>Candorbees</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
